9ccf1dd5f8df5e32fb7ad436f6f6b4c1ba92b3bf
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 23:14:27 -0800

    added solution to divisible by 400 test

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,11 @@
 def leap_year(year):
     if year % 4 == 0:
         if year % 100 == 0:
-            return( str(year) + " is not a leap year. ")
+            if year % 400 == 0:
+                return ( str(year) + " is a leap year. ")
+            else:
+                return ( str(year) + " is not a leap year. ")
         else:
-            return( str(year) + " is a leap year. ")
+            return ( str(year) + " is a leap year. ")
+    else:
+        return ( str(year) + " is not a leap year. ")
