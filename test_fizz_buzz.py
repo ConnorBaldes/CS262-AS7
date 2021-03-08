11874d5e7e4a6f44eee6e736c049cb6a263570f4
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 21:04:06 -0800

    Fiz Buzz initial commit. set up code and test file

---
+++ test_fizz_buzz.py
@@ -0,0 +1,14 @@
+import unittest
+import fizz_buzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizz_buzz(self):
+
+        
+
+
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity=2)
