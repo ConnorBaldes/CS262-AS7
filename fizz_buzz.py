fc03e5f1c9f7adf6680b6d73096613b2b26a51ad
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 22:40:25 -0800

    added solution to test four

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,8 +1,10 @@
 def fizz_buzz():
     numbers = ""
     for i in range(100):
-        if ((i+1) % 3) != 0:
+        if ((i+1) % 3) != 0 and ((i+1) % 5) != 0:
             numbers += str(i+1)
+        elif ((i+1) % 5) == 0:
+                numbers += "Buzz"
         else:
             numbers += "Fizz"
         if i != 99:
