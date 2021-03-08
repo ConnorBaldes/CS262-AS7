7af85fd9aa8a539111be1686714408cd673994be
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 22:26:13 -0800

    Added code solution to test three

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,7 +1,10 @@
 def fizz_buzz():
     numbers = ""
     for i in range(100):
-        numbers += str(i+1)
+        if ((i+1) % 3) != 0:
+            numbers += str(i+1)
+        else:
+            numbers += "Fizz"
         if i != 99:
             numbers +=" "
     return numbers
