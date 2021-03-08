0fc917b7a4369da6def2e5da4bfab286eac74823
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 23:10:34 -0800

    added solution to divisible by 100 test

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,6 @@
 def leap_year(year):
     if year % 4 == 0:
-        return( str(year) + " is a leap year. ")
+        if year % 100 == 0:
+            return( str(year) + " is not a leap year. ")
+        else:
+            return( str(year) + " is a leap year. ")
