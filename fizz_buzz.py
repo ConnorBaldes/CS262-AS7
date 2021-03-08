4bf0787581878ef24e9fbce533f4c0c0cfd818ac
ConnorBaldes <connorbaldes@gmail.com>
2021-03-07 22:49:18 -0800

    created solution to test four which is the final test

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,6 +3,8 @@ def fizz_buzz():
     for i in range(100):
         if ((i+1) % 3) != 0 and ((i+1) % 5) != 0:
             numbers += str(i+1)
+        elif ((i+1) % 3) == 0 and ((i+1) % 5) == 0:
+            numbers += "FizzBuzz"
         elif ((i+1) % 5) == 0:
                 numbers += "Buzz"
         else:
